e53cb71981f0740f950b2ecad495c2fec0c9e25f
Justin Shiao <justin19910401@gmail.com>
2013-11-08 23:49:13 +0800

    STM32 ADC first update.

---
+++ discoveryF4/discovery_demo/main.c
@@ -0,0 +1,262 @@
+/* Reference to stm32 discovery demo example.*/
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "usbd_hid_core.h"
+#include "usbd_usr.h"
+#include "usbd_desc.h"
+
+//Library config for this project!!!!!!!!!!!
+#include "stm32f4xx_conf.h"
+#include "stm32f4xx_adc.h"
+
+/** @addtogroup STM32F4-Discovery_Demo
+  * @{
+  */
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+
+#define TESTRESULT_ADDRESS         0x080FFFFC
+#define ALLTEST_PASS               0x00000000
+#define ALLTEST_FAIL               0x55555555
+#define BUFFERSIZE 128
+ 
+uint16_t ADCConvertedValues[BUFFERSIZE];
+
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+#ifdef USB_OTG_HS_INTERNAL_DMA_ENABLED
+  #if defined ( __ICCARM__ ) /*!< IAR Compiler */
+    #pragma data_alignment = 4   
+  #endif
+#endif /* USB_OTG_HS_INTERNAL_DMA_ENABLED */
+__ALIGN_BEGIN USB_OTG_CORE_HANDLE  USB_OTG_dev __ALIGN_END;
+  
+uint16_t PrescalerValue = 0;
+
+__IO uint32_t TimingDelay;
+__IO uint8_t DemoEnterCondition = 0x00;
+__IO uint8_t UserButtonPressed = 0x00;
+LIS302DL_InitTypeDef  LIS302DL_InitStruct;
+LIS302DL_FilterConfigTypeDef LIS302DL_FilterStruct;  
+__IO int8_t X_Offset, Y_Offset, Z_Offset  = 0x00;
+uint8_t Buffer[6];
+int ConvertedValue = 0; //Converted value readed from ADC
+
+/* Private function prototypes -----------------------------------------------*/
+static uint32_t Demo_USBConfig(void);
+//static void TIM4_Config(void);
+
+/* Private functions ---------------------------------------------------------*/
+int adc_convert();
+void adc_configure();
+
+/**
+  * @brief  Main program.
+  * @param  None
+  * @retval None
+  */
+int main(void)
+{
+  RCC_ClocksTypeDef RCC_Clocks;
+  
+  /* Initialize LEDs and User_Button on STM32F4-Discovery --------------------*/
+  STM_EVAL_PBInit(BUTTON_USER, BUTTON_MODE_EXTI); 
+  
+  STM_EVAL_LEDInit(LED4);
+  
+  /* SysTick end of count event each 10ms */
+  RCC_GetClocksFreq(&RCC_Clocks);
+  SysTick_Config(RCC_Clocks.HCLK_Frequency / 100);
+  
+    /* Turn on LEDs available on STM32F4-Discovery ---------------------------*/
+  STM_EVAL_LEDOn(LED4);
+
+  if (TimingDelay == 0x00)
+  {
+    /* Turn off LEDs available on STM32F4-Discovery ------------------------*/
+    STM_EVAL_LEDOff(LED4);
+    
+    /* Write PASS code at last word in the flash memory */
+    FLASH_ProgramWord(TESTRESULT_ADDRESS, ALLTEST_PASS);
+
+/* Try to test ADC.*/      
+  adc_configure();//Start configuration
+  
+    while(1){//loop while the board is working
+      ConvertedValue = adc_convert();//Read the ADC converted value
+      if (ConvertedValue > 2000)
+      {
+        STM_EVAL_LEDOn(LED4);
+        Delay(100);
+      }
+      else{
+        STM_EVAL_LEDOff(LED4);
+        Delay(100);
+      }
+    }
+
+/* Try to test ADC.*/
+  }
+  else{}
+}
+ 
+void adc_configure(){
+ ADC_InitTypeDef ADC_init_structure; //Structure for adc confguration
+ GPIO_InitTypeDef GPIO_initStructre; //Structure for analog input pin
+ //Clock configuration
+ RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1,ENABLE);//The ADC1 is connected the APB2 peripheral bus thus we will use its clock source
+ RCC_AHB1PeriphClockCmd(RCC_AHB1ENR_GPIOCEN,ENABLE);//Clock for the ADC port!! Do not forget about this one ;)
+ //Analog pin configuration
+ GPIO_initStructre.GPIO_Pin = GPIO_Pin_0;//The channel 10 is connected to PC0
+ GPIO_initStructre.GPIO_Mode = GPIO_Mode_AN; //The PC0 pin is configured in analog mode
+ GPIO_initStructre.GPIO_PuPd = GPIO_PuPd_NOPULL; //We don't need any pull up or pull down
+ GPIO_Init(GPIOC,&GPIO_initStructre);//Affecting the port with the initialization structure configuration
+ //ADC structure configuration
+ ADC_DeInit();
+ ADC_init_structure.ADC_DataAlign = ADC_DataAlign_Right;//data converted will be shifted to right
+ ADC_init_structure.ADC_Resolution = ADC_Resolution_12b;//Input voltage is converted into a 12bit number giving a maximum value of 4096
+ ADC_init_structure.ADC_ContinuousConvMode = ENABLE; //the conversion is continuous, the input data is converted more than once
+ ADC_init_structure.ADC_ExternalTrigConv = ADC_ExternalTrigConv_T1_CC1;// conversion is synchronous with TIM1 and CC1 (actually I'm not sure about this one :/)
+ ADC_init_structure.ADC_ExternalTrigConvEdge = ADC_ExternalTrigConvEdge_None;//no trigger for conversion
+ ADC_init_structure.ADC_NbrOfConversion = 1;//I think this one is clear :p
+ ADC_init_structure.ADC_ScanConvMode = DISABLE;//The scan is configured in one channel
+ ADC_Init(ADC1,&ADC_init_structure);//Initialize ADC with the previous configuration
+ //Enable ADC conversion
+ ADC_Cmd(ADC1,ENABLE);
+ //Select the channel to be read from
+ ADC_RegularChannelConfig(ADC1,ADC_Channel_10,1,ADC_SampleTime_144Cycles);
+}
+
+int adc_convert(){
+ ADC_SoftwareStartConv(ADC1);//Start the conversion
+ while(!ADC_GetFlagStatus(ADC1, ADC_FLAG_EOC));//Processing the conversion
+ return ADC_GetConversionValue(ADC1); //Return the converted data
+}
+
+/**
+  * @brief  Execute the demo application.
+  * @param  None
+  * @retval None
+  */
+
+
+/**
+  * @brief  Initializes the USB for the demonstration application.
+  * @param  None
+  * @retval None
+  */
+static uint32_t Demo_USBConfig(void)
+{
+  USBD_Init(&USB_OTG_dev,
+            USB_OTG_FS_CORE_ID,
+            &USR_desc, 
+            &USBD_HID_cb, 
+            &USR_cb);
+  
+  return 0;
+}
+
+/**
+  * @brief  Configures the TIM Peripheral.
+  * @param  None
+  * @retval None
+  */
+
+
+/**
+  * @brief  Inserts a delay time.
+  * @param  nTime: specifies the delay time length, in 10 ms.
+  * @retval None
+  */
+void Delay(__IO uint32_t nTime)
+{
+  TimingDelay = nTime;
+
+  while(TimingDelay != 0);
+}
+
+/**
+  * @brief  Decrements the TimingDelay variable.
+  * @param  None
+  * @retval None
+  */
+void TimingDelay_Decrement(void)
+{
+  if (TimingDelay != 0x00)
+  { 
+    TimingDelay--;
+  }
+}
+
+/**
+  * @brief  This function handles the test program fail.
+  * @param  None
+  * @retval None
+  */
+void Fail_Handler(void)
+{
+  /* Erase last sector */ 
+  FLASH_EraseSector(FLASH_Sector_11, VoltageRange_3);
+  /* Write FAIL code at last word in the flash memory */
+  FLASH_ProgramWord(TESTRESULT_ADDRESS, ALLTEST_FAIL);
+  
+  while(1)
+  {
+    /* Toggle Red LED */
+    STM_EVAL_LEDToggle(LED5);
+    Delay(5);
+  }
+}
+
+/**
+  * @brief  MEMS accelerometre management of the timeout situation.
+  * @param  None.
+  * @retval None.
+  */
+uint32_t LIS302DL_TIMEOUT_UserCallback(void)
+{
+  /* MEMS Accelerometer Timeout error occured during Test program execution */
+  if (DemoEnterCondition == 0x00)
+  {
+    /* Timeout error occured for SPI TXE/RXNE flags waiting loops.*/
+    Fail_Handler();    
+  }
+  /* MEMS Accelerometer Timeout error occured during Demo execution */
+  else
+  {
+    while (1)
+    {   
+    }
+  }
+  return 0;  
+}
+
+#ifdef  USE_FULL_ASSERT
+
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *   where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{ 
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
+#endif
+
+/**
+  * @}
+  */
+
+
+/******************* (C) COPYRIGHT 2011 STMicroelectronics *****END OF FILE****/
