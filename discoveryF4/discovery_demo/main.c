a3b09afc66861bced05fe26087abb66ec882a00d
Bo-cheng Jan <school510587@yahoo.com.tw>
2013-11-12 01:52:36 +0800

    adjust comment for adc_configure()

--- discoveryF4/discovery_demo/main.c
+++ discoveryF4/discovery_demo/main.c
@@ -118,25 +118,28 @@ void adc_configure(){
   ADC_InitTypeDef ADC_init_structure; //Structure for adc confguration
   GPIO_InitTypeDef GPIO_initStructre; //Structure for analog input pin
   //Clock configuration
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1,ENABLE);//The ADC1 is connected the APB2 peripheral bus thus we will use its clock source
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1,ENABLE);//ADC1 is connected to APB2 peripheral bus
   RCC_AHB1PeriphClockCmd(RCC_AHB1ENR_GPIOCEN,ENABLE);//Clock for the ADC port!! Do not forget about this one ;)
-  //Analog pin configuration
+  //Analog input pin configuration
   GPIO_initStructre.GPIO_Pin = GPIO_Pin_0;//The channel 10 is connected to PC0
   GPIO_initStructre.GPIO_Mode = GPIO_Mode_AN; //The PC0 pin is configured in analog mode
   GPIO_initStructre.GPIO_PuPd = GPIO_PuPd_NOPULL; //We don't need any pull up or pull down
-  GPIO_Init(GPIOC,&GPIO_initStructre);//Affecting the port with the initialization structure configuration
+  GPIO_Init(GPIOC,&GPIO_initStructre);
+
   //ADC structure configuration
-  ADC_DeInit();
-  ADC_init_structure.ADC_DataAlign = ADC_DataAlign_Right;//data converted will be shifted to right
-  ADC_init_structure.ADC_Resolution = ADC_Resolution_12b;//Input voltage is converted into a 12bit number giving a maximum value of 4096
+  ADC_DeInit();//reset all parameters to their default values
+  ADC_init_structure.ADC_DataAlign = ADC_DataAlign_Right;//converted data will be shifted to right
+  ADC_init_structure.ADC_Resolution = ADC_Resolution_12b;//Input voltage is converted into a 12-bit number whose maximum value is 4095
   ADC_init_structure.ADC_ContinuousConvMode = ENABLE; //the conversion is continuous, the input data is converted more than once
-  ADC_init_structure.ADC_ExternalTrigConv = ADC_ExternalTrigConv_T1_CC1;// conversion is synchronous with TIM1 and CC1 (actually I'm not sure about this one :/)
+  ADC_init_structure.ADC_ExternalTrigConv = ADC_ExternalTrigConv_T1_CC1;//use timer 1 capture/compare channel 1 for external trigger
   ADC_init_structure.ADC_ExternalTrigConvEdge = ADC_ExternalTrigConvEdge_None;//no trigger for conversion
-  ADC_init_structure.ADC_NbrOfConversion = 1;//I think this one is clear :p
-  ADC_init_structure.ADC_ScanConvMode = DISABLE;//The scan is configured in one channel
-  ADC_Init(ADC1,&ADC_init_structure);//Initialize ADC with the previous configuration
+  ADC_init_structure.ADC_NbrOfConversion = 1;//Number of used ADC channels
+  ADC_init_structure.ADC_ScanConvMode = DISABLE;//No scan (only one channel)
+  ADC_Init(ADC1,&ADC_init_structure);
  
+  // use channel 10 from ADC1, with sample time 144 cycles
   ADC_RegularChannelConfig(ADC1, ADC_Channel_10, 1, ADC_SampleTime_144Cycles);
+
   ADC_Cmd(ADC1,ENABLE);
 }
 
